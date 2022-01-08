4320c03d3c5cf835835881e356943e5be40f1bdc
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2022-01-09 01:18:16 +0600

    WIP: stop command

--- jeeves.py
+++ jeeves.py
@@ -4,13 +4,17 @@ import docker
 from os import path
 import json
 
+from docker import client
+
+
 class Service:
-    def __init__(self, tag: str, name: str, image: str, env: dict):
+    def __init__(self, tag: str, name: str, image: str, env: dict, volumes: list, ports: dict):
         self.tag = tag
         self.name = name
         self.image = image
         self.env = env
-
+        self.ports = ports
+        self.volumes = volumes
 
 
 @click.group()
@@ -32,15 +36,54 @@ def start(name):
                 name=service_configuration['name'],
                 image=service_configuration['image'],
                 env=service_configuration['env'],
+                ports=service_configuration['ports'],
+                volumes=service_configuration['volumes'],
             )
 
-            client.containers.run(image = f"{service.image}:{service.tag}", environment = service.env, detach=True)
+            labels = {
+                'jeeves': f"{service.name}--{service.tag}--{service.tag}--{service.ports['destination']}"
+            }
+
+            volumes = {
+                service.volumes['name']: {
+                    'bind': service.volumes['destination'],
+                    'mode': 'rw'
+                }
+            }
+
+            ports = {
+                service.ports['source']: service.ports['destination']
+            }
+
+            client.containers.run(
+                image=f"{service.image}:{service.tag}", environment=service.env, ports=ports, volumes=volumes, labels=labels, detach=True)
         echo(f"{name} started succesfully!")
     else:
-        echo(f"{name} doesn't exist")
+        echo(f"{name} is not a valid service name")
+
+
+@click.command()
+@click.argument('name')
+def stop(name):
+    client = docker.from_env()
+
+    containers = client.containers.list(filters={
+        'label': 'jeeves',
+        'status': 'running'
+    })
+
+    filtered_containers = filter(
+        lambda container: container.labels['jeeves'].split('--')[0] == name, containers)
+
+    for container in filtered_containers:
+        container.stop()
+        echo(f"{container.name} stopped succesfully!")
+        container.remove(v=True)
+        echo(f"{container.name} removed succesfully!")
 
 
 jeeves.add_command(start)
+jeeves.add_command(stop)
 
 if __name__ == "__main__":
     jeeves()
